8bb02a5acc82a4e81eeadea906e594ca5be844b2
h4nn4hm0rr15 <hannah.morris@jetstack.io>
2018-03-22 22:21:55 +0000

    consolidate all scripts into one, make map display based on api response, remove unused elements

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('map.html')
+
+@app.route('/get_weather', methods=['POST'])
+def weather():
+    city_name = request.form['city_name']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+'&appid=16a720ad9ccb6dc7f6c28e93aacc2962')
+    json_object = r.json()
+    weather = str(json_object['weather'][0]['main'])
+    temp_k = float(json_object['main']['temp'])
+    temp_f = (temp_k - 273.15) * 1.8 + 32
+    temp_c = (temp_f - 32) * 5/9
+    wind_speed = float(json_object['wind']['speed'])
+    city = str(json_object['name'])
+
+    latitude = float(json_object['coord']['lat'])
+    longitude = float(json_object['coord']['lon'])
+
+    return render_template('2index.html', main=weather, temp=temp_c, speed=wind_speed, city=city, longitude=longitude, latitude = latitude)
 
 # @app.route('/map/')
 # def map():
