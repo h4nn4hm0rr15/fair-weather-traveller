cd3f667e61c89896a6ca2ee71fc5db17999e436f
h4nn4hm0rr15 <hannah.morris@jetstack.io>
2018-03-22 21:26:50 +0000

    get css and static images working with flask

--- WeatherApp/2weather.py
+++ WeatherApp/2weather.py
@@ -12,9 +12,10 @@ def weather():
     weather = str(json_object['weather'][0]['main'])
     temp_k = float(json_object['main']['temp'])
     temp_f = (temp_k - 273.15) * 1.8 + 32
+    temp_c = (temp_f - 32) * 5/9
     wind_speed = float(json_object['wind']['speed'])
     city = str(json_object['name'])
-    return render_template('2temperature.html', main=weather, temp=temp_f, speed=wind_speed, city=city)
+    return render_template('2temperature.html', main=weather, temp=temp_c, speed=wind_speed, city=city)
 
 
 @app.route('/')
